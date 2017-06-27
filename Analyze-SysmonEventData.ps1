41f7595b5e6fd59914b08501c9023e70a401988a
jokezone <jokezone@gmail.com>
2017-06-27 17:48:14 +0200

    Update to analyze past 3 days of events

--- Analyze-SysmonEventData.ps1
+++ Analyze-SysmonEventData.ps1
@@ -3,7 +3,7 @@
 foreach ($ID in (1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,255))
 {
     $events = @()
-    $events = Get-SysMonEventData -EventId $ID -MaxEvents 20 -Path .\SYSMON-Export-Events.evtx -ErrorAction SilentlyContinue
-    if ($events.count -ne "0"){$events | Export-Csv -NoTypeInformation -Path .\Sysmon_$($ID)_$($events[0].EventType).csv}
+    $events = Get-SysMonEventData -EventId $ID -EndTime (Get-Date) -StartTime (Get-Date).AddDays(-3) -ErrorAction SilentlyContinue
+    if ($events.count -ne "0"){$events | Export-Csv -NoTypeInformation -Path .\Sysmon_$($ID)_$($events[0].EventType)_$($events.count)_Events.csv}
     Write-Host "$($events.count) Sysmon Event ID $ID events found."
 }
