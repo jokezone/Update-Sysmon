741fbf49c43f8bb512aa789348854f764a1fbb3a
jokezone <jokezone@gmail.com>
2017-10-02 14:58:52 +0200

    Adding WMI activity eventIDs

--- Analyze-SysmonEventData.ps1
+++ Analyze-SysmonEventData.ps1
@@ -1,6 +1,6 @@
 . .\Get-SysmonEventData.ps1
 
-foreach ($ID in (1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,255))
+foreach ($ID in (1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,255))
 {
     $events = @()
     $events = Get-SysMonEventData -EventId $ID -EndTime (Get-Date) -StartTime (Get-Date).AddDays(-3) -ErrorAction SilentlyContinue
