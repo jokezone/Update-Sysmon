2be67444112619591d8c055916144b3c6e02bf55
jokezone <jokezone@gmail.com>
2020-01-27 22:53:26 +0100

    Adding domain launcher script

---
+++ Update-SysmonDomainLauncher.ps1
@@ -0,0 +1,75 @@
+<#
+.SYNOPSIS
+    This script will set the Update-Sysmon parameters based on the domain computer account role and group membership.
+    Author: Thomas Connell
+#>
+
+$FileShare = "\\FILESERVER\ShareName"
+$RunDir = $PSScriptRoot
+$LogDir = $env:TEMP
+$ConfigFile = $null
+$DelayExecution = 10
+
+if ($DelayExecution -gt 0)
+{
+    $delay = Get-Random -Minimum 1 -Maximum $DelayExecution
+    Write-Host "$(Get-Date): Delaying execution for $delay minutes"
+    Start-Sleep -Seconds ($delay * 60)
+}
+
+try
+{
+    # Query AD for group membership of computer
+    $domainDN = ([ADSI]"").distinguishedName
+    $root = [ADSI]"LDAP://$domainDN"
+    $search = [adsisearcher]$root
+    $Search.Filter = "(&(SamAccountName=$ENV:COMPUTERNAME$))"
+    $computer = $Search.FindOne()
+    $computerproperties = $computer.Properties
+    [array]$groups = $computerproperties.memberof
+
+    # Identify the computer role
+    $Role = (Get-WmiObject Win32_ComputerSystem).DomainRole
+    if ($Role -eq 1) {$OSType = "workstation"}
+    if ($Role -eq 3) {$OSType = "memberserver"}
+    if ($Role -ge 4) {$OSType = "domaincontroller"}
+
+    # Set script parameters
+    $groupToMatch = "Sysmon-Testing-CurrentVersion"
+    if($groups -match $groupToMatch)
+    {
+        $ConfigFile = "Config\sysmonconfig-$OSType-testing.xml"
+        $RunDir = $PSScriptRoot + "\Testing-CurrentVersion"
+        $UninstallMethod = "Graceful"
+        $LogDir = $FileShare + "\Sysmon-Logs\Testing-CurrentVersion"
+    }
+    $groupToMatch = "Sysmon-Verbose"
+    if($groups -match $groupToMatch)
+    {
+        $ConfigFile = "Config\sysmonconfig-$OSType-verbose.xml"
+        $RunDir = $PSScriptRoot + "\Testing-CurrentVersion"
+        $UninstallMethod = "Graceful"
+        $LogDir = $FileShare + "\Sysmon-Logs\Verbose"
+    }
+    $groupToMatch = "Sysmon-Testing-NewVersion"
+    if($groups -match $groupToMatch)
+    {
+        $ConfigFile = "Config\sysmonconfig-$OSType-testing.xml"
+        $RunDir = $PSScriptRoot + "\Testing-NewVersion"
+        $UninstallMethod = "Force"
+        $LogDir = $FileShare + "\Sysmon-Logs\Testing-NewVersion"
+    }
+    if (-not($ConfigFile))
+    {
+        $ConfigFile = "Config\sysmonconfig-$OSType-production.xml"
+        $UninstallMethod = "Graceful"
+    }
+}
+catch
+{
+    Write-Host "$(Get-Date): Unable to query AD to dynamically set a configuration file."
+}
+
+# Run Update-Sysmon using calculated parameters
+. "$RunDir\Update-Sysmon.ps1"
+Update-Sysmon -RunDir $RunDir -ConfigFile $ConfigFile -UninstallMethod $UninstallMethod -LogDir $LogDir -Verbose
