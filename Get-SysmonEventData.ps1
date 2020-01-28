694ad81440ebf01c756631af49446f21533b4762
jokezone <jokezone@gmail.com>
2020-01-28 19:16:54 +0100

    Update Get-SysmonEventData.ps1
    Added eventID 22 for DNS Query events

--- Get-SysmonEventData.ps1
+++ Get-SysmonEventData.ps1
@@ -19,7 +19,7 @@ function Get-SysmonEventData {
             ParameterSetName='ID',
             ValueFromPipelineByPropertyName=$true,
             Position=0)]
-        [ValidateSet(1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,255)]
+        [ValidateSet(1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,255)]
         [Int32[]]
         $EventId,
         
