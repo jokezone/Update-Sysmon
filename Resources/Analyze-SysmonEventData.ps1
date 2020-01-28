9249e53385bb1f1b0d6d5fe4ea1d76b450471291
jokezone <jokezone@gmail.com>
2020-01-29 00:33:17 +0100

    Added synopsis

--- Resources/Analyze-SysmonEventData.ps1
+++ Resources/Analyze-SysmonEventData.ps1
@@ -1,3 +1,8 @@
+<#
+.SYNOPSIS
+    This script will generate a CSV file for each type of Sysmon event logged. This can be used to improve your configuration file content.
+#>
+
 . .\Get-SysmonEventData.ps1
 
 foreach ($ID in (1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,255))
